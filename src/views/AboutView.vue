b1ff8509651b47d670ff169650155bbf56c61863
LJKhnor <135867702+LJKhnor@users.noreply.github.com>
2025-01-28 14:20:42 +0100

    fix typo in about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,8 +2,10 @@
   <div class="about">
     <h1>About Me</h1>
     <p>
-      Welcome to my portfolio! My name is Joachim Lejeune, and I am a passionate developer specializing in backend and frontend technologies. This site showcases my journey, skills, and projects.
+      Welcome to my gw2 pvp stats project!
     </p>
+    <p>My name is Joachim, and I am a passionate developer specializing in backend and frontend technologies.
+      This site showcases my journey, skills, and projects.</p>
     <section class="skills">
       <h2>My Skills</h2>
       <ul>
@@ -25,7 +27,7 @@
         Feel free to reach out to me for collaboration or to learn more about my work!
       </p>
       <ul>
-        <li >Github: <a href="https://github.com/LJKhnor">LJKhnor</a></li>
+        <li >Github: <a href="https://github.com/LJKhnor" target="_blank">LJKhnor</a></li>
         <li>Email: <a href="mailto:lejeune.joachim.dev@gmail.com">lejeune.joachim.dev@gmail.com</a></li>
       </ul>
     </section>
